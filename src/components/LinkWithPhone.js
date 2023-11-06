8ac76bb4486fd394456413b6d52ff28d73d597c1
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-11-06 17:45:08 +0600

    added country api

--- src/components/LinkWithPhone.js
+++ src/components/LinkWithPhone.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LinkWithPhone = ({setLinkWith}) => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+      const getCountries = async () => {
+        try{
+          const response = await fetch(`https://restcountries.com/v2/all`);
+          const data = await response.json()
+          setCountries(data.slice(0, 150))
+        }catch(error){
+          console.log(error)
+        }
+      }
+
+    getCountries()
+    console.log(countries)
+  }, []);
   return (
-    <div className="pb-[60px] px-[60px] text-center">
-      <div className='text-[#41525d] text-[26px] font-light mb-4'>Enter phone number</div>
-      <div className="text-[#8696a0] text-base mb-8">Select a country and enter your WhatsApp phone number.</div>
+    <div className="pb-[60px] px-[60px] ">
+      <div className='text-[#41525d] text-[26px] font-light mb-4 text-center'>Enter phone number</div>
+      <div className="text-[#8696a0] text-base mb-8 text-center">Select a country and enter your WhatsApp phone number.</div>
 
       <div>
-        <select name="" id="" className='border border-borderDefault rounded-md h-[53px] w-[266px] px-6' placeholder='Bangladesh'>
+        <div className='relative border border-borderDefault rounded-md h-[53px] w-[266px] mx-auto cursor-pointer'>
+          <div onClick={() => setOpenDropdown(openDropdown => !openDropdown)} className='w-full h-full flex items-center'>
+            <div className='px-6'>Bangladesh {countries?.length}</div>  
+          </div>
+          {
+            openDropdown && <div className="country-dropdown-box p-4 w-full bg-white absolute top-16 z-40">
+              {
+                countries?.length && 
+                countries?.sort((a,b) => a?.name.localeCompare(b?.name)).map((country) => (
+                  <div key={country.name}>
+                    {country.name} 
+                    <span className='text-red-400 font-bold'>{country?.callingCodes[0]}</span>
+                    <span className='text-blue-400 font-bold'>{country?.nativeName}</span>
+                    <img src={country?.flags?.svg} alt="" className='h-4' />
+                    </div>
+                   
+                ))
+              }
+            </div>
+          }
+        </div>
+        {/* <select name="" id="" className='border border-borderDefault rounded-md h-[53px] w-[266px] px-6' placeholder='Bangladesh'>
           <option value="" selected>Bangladesh</option>
-        </select>
+        </select> */}
         <div className='border border-borderDefault rounded-md h-[53px] w-[266px] px-6 mx-auto mt-3 flex items-center mb-8'>
           +880
         </div>
         <button className='bg-[#008069] text-white h-10 w-[90px] flex items-center justify-center text-sm uppercase rounded font-semibold mx-auto tracking-wide'>Next</button>
       </div>
-      <button onClick={() => setLinkWith('link-with-qr-code')} className='text-[#008069] text-[17px] pt-[50px]'>Link with QR code</button>
+      <div className='text-center'><button onClick={() => setLinkWith('link-with-qr-code')} className='text-[#008069] text-[17px] pt-[50px]'>Link with QR code</button></div>
     </div>
   );
 };
